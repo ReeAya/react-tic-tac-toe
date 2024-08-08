91344d9d4cede868a31b77d314630fc0638582e6
reezan_s <reezansmith@gmail.com>
2024-08-08 13:55:16 +0900

    Added state to handle player name, name change etc

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
 
   function handleEditClickEvent() {
     setIsEditing((editing) => !editing);
   }
 
+  function handleChange(e) {
+    setName(e.target.value);
+  }
   let playerName = <span className={"player-name"}>{name}</span>;
   let bthCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" value={name} required></input>;
+    playerName = (
+      <input type="text" value={name} onChange={handleChange} required></input>
+    );
     bthCaption = "Save";
   }
   return (
